1c399e6b942dca554071e2db75bb332e97bd6785
Ewe Tek Min <tekminewe@users.noreply.github.com>
2024-07-23 19:40:38 +0100

    feat: update post item

--- src/components/post-item/post-item.tsx
+++ src/components/post-item/post-item.tsx
@@ -2,6 +2,8 @@ import { Box } from "@radix-ui/themes";
 import { Caption } from "../caption";
 import { Flex } from "../flex";
 import { Text } from "../text";
+import { forwardRef } from "react";
+import { cn } from "../utils";
 
 export interface PostItemProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
@@ -25,33 +27,35 @@ export interface PostItemProps extends React.HTMLAttributes<HTMLDivElement> {
   date: string;
 }
 
-export const PostItem = ({
-  title,
-  summary,
-  imageUrl,
-  date,
-  ...props
-}: PostItemProps) => {
-  return (
-    <div {...props}>
-      <Flex gap="4">
-        <Box className="flex-shrink-0" height="256px" width="256px">
-          <img
-            src={imageUrl}
-            alt={title}
-            width="100%"
-            height="100%"
-            className="object-cover w-full h-full rounded-lg border"
-          />
-        </Box>
-        <div>
-          <Caption>{date}</Caption>
-          <Text size="5" weight="bold" my="4">
-            {title}
-          </Text>
-          <Text>{summary}</Text>
-        </div>
-      </Flex>
-    </div>
-  );
-};
+export const PostItem = forwardRef<HTMLDivElement, PostItemProps>(
+  ({ title, summary, imageUrl, date, ...props }, ref) => {
+    return (
+      <div
+        {...props}
+        className={cn(props.className, "cursor-pointer")}
+        ref={ref}
+      >
+        <Flex gap="4">
+          <Box className="flex-shrink-0" height="256px" width="256px">
+            <img
+              src={imageUrl}
+              alt={title}
+              width="100%"
+              height="100%"
+              className="object-cover w-full h-full rounded-lg border"
+            />
+          </Box>
+          <div>
+            <Caption>{date}</Caption>
+            <Text size="5" weight="bold" my="4">
+              {title}
+            </Text>
+            <Text>{summary}</Text>
+          </div>
+        </Flex>
+      </div>
+    );
+  }
+);
+
+PostItem.displayName = "PostItem";
