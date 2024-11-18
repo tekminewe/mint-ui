b34cf8a536d8228e718dadffbbb5befeec739479
Ewe Tek Min <ewe.tekmin@outlook.com>
2024-11-18 16:46:19 +0000

    feat: add heading

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -5,7 +5,7 @@ export const Text = forwardRef<HTMLSpanElement, TextProps>((props, ref) => {
   return (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    <RadixText ref={ref} {...props} as={props.as ?? "p"} />
+    <RadixText ref={ref} {...props} as={props.as} />
   );
 });
 
