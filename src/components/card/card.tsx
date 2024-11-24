c6b696c7e31f59f1ac6156d38ba64900171d65ff
Ewe Tek Min <ewe.tekmin@outlook.com>
2024-11-24 13:49:13 +0000

    feat: add shadow to card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,3 +1,6 @@
-import { Card as RadixCard } from '@radix-ui/themes';
+import { CardProps, Card as RadixCard } from "@radix-ui/themes";
+import { cn } from "../utils";
 
-export const Card = RadixCard;
+export const Card = (props: CardProps) => {
+  return <RadixCard {...props} className={cn("shadow-3", props.className)} />;
+};
