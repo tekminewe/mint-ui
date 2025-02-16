efadb6e76443127a9dbaccf714b8f5deb6022057
Ewe Tek Min <ewe.tekmin@outlook.com>
2025-02-16 15:45:58 +0000

    feat: add sidebar

--- src/components/rich-text-editor/node-command.tsx
+++ src/components/rich-text-editor/node-command.tsx
@@ -1,6 +1,6 @@
 import { Command } from "cmdk";
-import { Heading1, Heading2, Heading3, Image } from "lucide-react";
 import { NodeCommandItem } from "./node-command-item";
+import { LuHeading1, LuHeading2, LuHeading3, LuImage } from "react-icons/lu";
 
 export type ItemType =
   | "heading-1"
@@ -20,31 +20,31 @@ export const NodeCommand = ({ onItemClick }: NodeCommandProps) => {
       <Command.List>
         <Command.Group>
           <NodeCommandItem
-            icon={<Heading1 size={24} />}
+            icon={<LuHeading1 size={24} />}
             title="Heading 1"
             description="Big section heading"
             onClick={() => onItemClick?.({ id: "heading-1" })}
           />
           <NodeCommandItem
-            icon={<Heading2 size={24} />}
+            icon={<LuHeading2 size={24} />}
             title="Heading 2"
             description="Medium section heading"
             onClick={() => onItemClick?.({ id: "heading-2" })}
           />
           <NodeCommandItem
-            icon={<Heading3 size={24} />}
+            icon={<LuHeading3 size={24} />}
             title="Heading 3"
             description="Small section heading"
             onClick={() => onItemClick?.({ id: "heading-3" })}
           />
           <NodeCommandItem
-            icon={<Image size={24} />}
+            icon={<LuImage size={24} />}
             title="Image"
             description="Upload an image"
             onClick={() => onItemClick?.({ id: "image" })}
           />
           <NodeCommandItem
-            icon={<Image size={24} />}
+            icon={<LuImage size={24} />}
             title="Code Block"
             description="Insert a code snippet"
             onClick={() => onItemClick?.({ id: "code-block" })}
